add95304aa7cc4fca5493a283180dedfe274dff6
MERCY OGALO <143324456+mercyogalo@users.noreply.github.com>
2025-08-27 22:35:30 +0300

    Added the backend folder and the files required for authentication

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express=require("express");
+const cors=require("cors");
+const dotenv=require("dotenv");
+const mongoose=require("mongoose");
+const app=express();
+
+app.use(express.json());
+app.use(cors());
+dotenv.config();
+
+const PORT=process.env.PORT || 5000;
+
+mongoose
+.connect(process.env.MONGO_URI,{})
+.then(()=>console.log(`Mongodb running`))
+.catch((err)=>console.log(`The error is ${err}`))
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`)
+})
