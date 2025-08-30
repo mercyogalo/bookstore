2b031c433a302ebb6dd3c0d86fcf35b1d5a44c2d
MERCY OGALO <143324456+mercyogalo@users.noreply.github.com>
2025-08-30 22:42:01 +0300

    Added the favorites model data

--- backend/models/Favorite.js
+++ backend/models/Favorite.js
@@ -1,5 +1,23 @@
 const mongoose=require("mongoose");
+const Reviewer=require("./User");
+
 
 const favoriteSchema=new mongoose.Schema({
-    
+    reviewer:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Reviewer",
+        required:true
+    },
+    book:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Book",
+        required:true
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
 })
+
+
+const Favorite=("Favorite", favoriteSchema)
